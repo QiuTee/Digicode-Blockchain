dfba987ee99a536e1769a27110bc830b3193c920
QiuTee <117626385+QiuTee@users.noreply.github.com>
2024-08-26 02:24:06 +0700

    Update settings.py

--- Backend/backend_project/backend_project/settings.py
+++ Backend/backend_project/backend_project/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000","https://cos-30049-team-project.vercel.app" , "https://digicode-blockchain-zdru.vercel.app" 
+    "http://localhost:3000","https://cos-30049-team-project.vercel.app" , "https://digicode-blockchain.vercel.app/" 
 ]
 
 REST_FRAMEWORK = {
